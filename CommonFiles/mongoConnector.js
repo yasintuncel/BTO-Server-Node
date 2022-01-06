c1e580566a9c949dc226ae0f1cb252c6a12c34ba
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-07 00:08:29 +0300

    Creation of the mongoConnector is done. close #1

---
+++ CommonFiles/mongoConnector.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const mongoConnector = async function (uri) {
+    try {
+        //mongoose.Promise = global.Promise;
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true,
+            // useFindAndModify: false,
+        });
+        return {
+            isConnected: true,
+            message: 'Successfully connected to MongoDB.'
+        };
+    } catch (e) {
+        return {
+            isConnected: false,
+            message: 'Database connection failed. Error: ' + e
+        };
+    }
+};
+module.exports = mongoConnector;
