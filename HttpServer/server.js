29c9bc865b9cb16da08dd2edb47f704e8ceef0e7
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-07 23:42:02 +0300

    creation of http server is done. close #2

---
+++ HttpServer/server.js
@@ -0,0 +1,19 @@
+const mongoConnector = require('common/mongoConnector');
+const config = require('./config');
+const createHttpServer = require('./createHttpServer');
+//
+const start = async function () {
+    console.log('Starting new Server... ');
+    let dbStatus = await mongoConnector(config.mongoUri);
+    console.log(dbStatus.message);
+    //
+    if (dbStatus.isConnected) {
+        createHttpServer(config.httpPort);
+    }
+    else {
+        console.log('Exiting now!');
+        process.exit(1);
+    }
+};
+
+start();
