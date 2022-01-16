98550b4bf9f95dfd5f205eedb663c1fb532293fc
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-16 15:28:12 +0300

    creation of userSeeder done. close #30

--- HttpServer/server.js
+++ HttpServer/server.js
@@ -1,6 +1,7 @@
 const mongoConnector = require('common/database/mongoConnector');
 const config = require('./config');
 const createHttpServer = require('./createHttpServer');
+const userSeeder = require('./seeders/userSeeder');
 //
 const start = async function () {
     console.log('Starting new Server... ');
@@ -8,6 +9,7 @@ const start = async function () {
     console.log(dbStatus.message);
     //
     if (dbStatus.isConnected) {
+        await userSeeder();
         createHttpServer(config.httpPort);
     }
     else {
