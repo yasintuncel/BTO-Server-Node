9c5882763967f9b84aaee19ad84e2c37d06a3884
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-17 17:41:16 +0300

    set user activity while creating a new game

--- HttpServer/routes/api/game/createGame.js
+++ HttpServer/routes/api/game/createGame.js
@@ -1,5 +1,5 @@
 const createNewGame = require('common/database/functions/createNewGame');
-
+const { User } = require('common/database//models/user');
 const createGame = async function (req, res) {
 
     var { playCount, gold, isPrivate } = req.body;
@@ -8,11 +8,24 @@ const createGame = async function (req, res) {
         res.status(400).send("All fields are required");
     }
     else {
-        const gameStatus = await createNewGame(req.userId, playCount, gold, isPrivate);
+        const userId = req.userId;
+        const gameStatus = await createNewGame(userId, playCount, gold, isPrivate);
 
         if (gameStatus.isCreated) {
-            // change user activity for in-game
-            res.status(200).json({ game: gameStatus.game.toObject() });
+            try {
+                let user = await User.findByIdAndUpdate(userId, {
+                    'activity.isInGame': true,
+                    'activity.gameId': gameStatus.game._id.toString(),
+                });
+                // user.activity.isInGame = true;
+                // user.activity.gameId = gameStatus.game._id.toString();
+                // await user.save();
+
+                res.status(200).json({ game: gameStatus.game.toObject() }); // Todo create map -> game for users
+            }
+            catch (e) {
+                res.status(503).json({ 'error': e });
+            }
         }
         else {
             res.status(500).json(gameStatus.message);
