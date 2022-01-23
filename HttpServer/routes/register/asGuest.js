2ce451080c186442dbd51046aef06dfc43f28c2c
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-23 13:31:28 +0300

    blocking registration with uid is done. close #36

--- HttpServer/routes/register/asGuest.js
+++ HttpServer/routes/register/asGuest.js
@@ -1,6 +1,8 @@
 const config = require('../../config');
 
 const createNewGuestUser = require('common/database/functions/createNewGuestUser');
+const { User } = require('common/database/models/user');
+const tokenGenerator = require('common/utils/tokenGenerator');
 
 const asGuest = async function (req, res) {
     const { nickName } = req.body;
@@ -8,14 +10,27 @@ const asGuest = async function (req, res) {
         res.status(400).send("A nick name is required");
         return;
     }
-
-    const newUserStatus = await createNewGuestUser(nickName, config.tokenScreetKey, config.tokenDuration);
-    console.log(newUserStatus.message + ` Request ip: + ${req.ip}`);
-    if (newUserStatus.isCreated) {
-        res.status(200).json({ "token": newUserStatus.token });
+    try {
+        let registeredUser = await User.findOne({ 'uid': nickName });
+        if (registeredUser) {
+            let newToken = tokenGenerator(registeredUser._id, config.tokenScreetKey, config.tokenDuration);
+            registeredUser.token = newToken;
+            registeredUser.save();
+            res.status(200).json({ "token": newToken });
+        }
+        else {
+            const newUserStatus = await createNewGuestUser(nickName, config.tokenScreetKey, config.tokenDuration);
+            console.log(newUserStatus.message + ` Request ip: + ${req.ip}`);
+            if (newUserStatus.isCreated) {
+                res.status(200).json({ "token": newUserStatus.token });
+            }
+            else {
+                res.status(500).json(newUserStatus.message);
+            }
+        }
     }
-    else {
-        res.status(500).json(newUserStatus.message);
+    catch (e) {
+        res.status(500).json({ 'error': 'an unkown error' });
     }
 };
 
