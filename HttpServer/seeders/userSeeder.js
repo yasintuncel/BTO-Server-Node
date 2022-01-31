c9dee9c4ae1b21d1e11ddb70a6419698fb891c35
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-01-31 22:59:11 +0300

    editing the userSeeder for friend system is done

--- HttpServer/seeders/userSeeder.js
+++ HttpServer/seeders/userSeeder.js
@@ -2,24 +2,46 @@ const config = require('../config');
 const { User } = require('common/database/models/user');
 const createNewGuestUser = require('common/database/functions/createNewGuestUser');
 const { Activity } = require('common/database/models/user/activity');
+const { Friend } = require('common/database/models/user/friend');
 
 const seedUsersInformation = [
     {
         nickName: "Yasin",
+        friends: ['Akdeniz', 'Electronic', 'Coder']
     },
     {
         nickName: "Akdeniz",
+        friends: ['Yasin']
     },
     {
         nickName: "Electronic",
+        friends: ['Yasin', 'Engineer']
     },
     {
         nickName: "Engineer",
+        friends: ['Electronic']
     },
     {
         nickName: "Coder",
+        friends: ['Yasin', 'Engineer']
     }
 ];
+
+const addFriends = async function () {
+    for (const userInfo of seedUsersInformation) {
+        for (const friendName of userInfo.friends) {
+            let user = await User.findOne({ 'nickName': userInfo.nickName });
+            if (user.friends.length !== userInfo.friends.length) {
+                let friend = await User.findOne({ 'nickName': friendName });
+                let fSchema = Friend();
+                fSchema.user = friend;
+                user.friends.push(fSchema);
+                await user.save();
+                console.log('friend added.');
+            }
+        }
+    }
+}
 
 const userSeeder = async function () {
     for (let i = 0; i < seedUsersInformation.length; i++) {
@@ -41,7 +63,7 @@ const userSeeder = async function () {
             }
         }
     };
-
+    await addFriends();
     let allUsers = await User.find({});
     allUsers.forEach(user => {
         user.activity = Activity();
