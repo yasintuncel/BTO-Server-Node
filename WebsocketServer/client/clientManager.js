4a22bffd4366ee566fa599af5fce642a419ce3aa
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-02-19 11:20:33 +0300

    userClients added

--- WebsocketServer/client/clientManager.js
+++ WebsocketServer/client/clientManager.js
@@ -1,13 +1,26 @@
+const { WebSocket } = require('ws');
 const onUserConnect = require('./user/onUserConnect');
 const onUserDisconnect = require('./user/onUserDisconnect');
+
+let userClients = {};
 
 const clientManager = {
     handleClient: async function (socket, request) {
         const userId = request.user.id.toString();
         const connectionStatus = await onUserConnect(socket, userId);
-        //
-        if (!connectionStatus.isValidUser) return;
-        //
+
+        if (!connectionStatus.isValidUser)
+            return;
+        let user = connectionStatus.user;
+
+        let userClient = {
+            userId: userId,
+            user: user,
+            socket: socket,
+            isAlive: true,
+        };
+        userClients[userId] = userClient;
+
         socket.on('message', function incoming(message) {
             console.log(user.nickName + '>_ ' + JSON.parse(message).message);
         });
